55881ab45fdf8f92a480fcbee2e3297d09d77d73
Max <43404248+impaex@users.noreply.github.com>
2022-07-18 15:29:42 +0200

    Moved DB model

--- index.js
+++ index.js
@@ -20,6 +20,27 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 	storage: 'database.sqlite',
 });
 
+// Assign models
+const Servers = sequelize.define('servers', {
+	ip: {
+		type: Sequelize.STRING,
+		primaryKey: true,
+	},
+	port: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+	},
+	name: Sequelize.TEXT,
+	tamingRate: Sequelize.INTEGER,
+	breedingRate: Sequelize.INTEGER,
+	harvestRate: Sequelize.INTEGER,
+	XPRate: Sequelize.INTEGER,
+	weekendTamingRate: Sequelize.INTEGER,
+	weekendBreedingRate: Sequelize.INTEGER,
+	weekendHarvestRate: Sequelize.INTEGER,
+	WeekendXPRate: Sequelize.INTEGER,
+})
+
 // Load commands
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
