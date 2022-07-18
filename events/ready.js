26992f3a08830156fd5bd534a16272ff08c10929
Max <43404248+impaex@users.noreply.github.com>
2022-07-18 18:13:08 +0200

    Removed old stuff from ready event

--- events/ready.js
+++ events/ready.js
@@ -2,8 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		Servers.sync();
-		console.log("Synced the Servers database.");
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
